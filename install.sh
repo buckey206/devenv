234fbac8c45411eaa98057fe715561f3595dc2c8
buckey206 <buckey206@gmail.com>
2015-11-01 01:17:31 -0800

    added install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-sudo apt-get install git vim tmux
+sudo apt-get install vim tmux
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+ln -s ~/devenv/vimrc ~/.vimrc
+ln -s ~/devenv/tmux.conf ~/.tmux.conf
 
