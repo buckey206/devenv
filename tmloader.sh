ca971d7f4101f5fb2cb84b1457b43acebef0179f
Jared <buckey206@gmail.com>
2015-11-05 04:32:56 -0500

    fixed indentation in vimrc

--- tmloader.sh
+++ tmloader.sh
@@ -6,14 +6,14 @@ tmux split-window -t dev:2 -v
 tmux split-window -t dev:2.2 -h
 tmux select-layout -t dev:2 main-horizontal
 tmux rename-window python
-tmux send-keys -t dev:2.1 "cd ~/Dropbox/python" "Enter"
-tmux send-keys -t dev:2.2 "cd ~/Dropbox/python" "Enter"
-tmux send-keys -t dev:2.3 "cd ~/Dropbox/python" "Enter"
+tmux send-keys -t dev:2.1 "cd ~/Dropbox/python/pythonProjects" "Enter"
+tmux send-keys -t dev:2.2 "cd ~/Dropbox/python/pythonProjects" "Enter"
+tmux send-keys -t dev:2.3 "cd ~/Dropbox/python/pythonProjects" "Enter"
 tmux new-window -t dev:3
 tmux select-window -t dev:3
 tmux split-window -t dev:3 -h
 tmux rename-window webdev
-tmux send-keys -t dev:3.1 "cd /var/www/" "Enter"
-tmux send-keys -t dev:3.2 "cd /var/www/" "Enter"
+tmux send-keys -t dev:3.1 "cd /var/www/" "Enter" "vim ." "Enter"
+tmux send-keys -t dev:3.2 "cd /var/www/" "Enter" "vim ." "Enter"
 tmux select-window -t dev:1
 tmux attach -t dev
--- vimrc
+++ vimrc
@@ -139,6 +139,12 @@ filetype on
 filetype plugin on
 filetype indent on
 
+"show existing tab with 4 spaces width
+set tabstop = 4
+"when indenting with '>', use 4 spaces width
+set shiftwidth=4
+" on pressing tab, insert 4 spaces
+set expandtab
 " reload files changed outside vim
 set autoread         
 
