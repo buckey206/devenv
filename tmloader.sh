8d691f1029c6980b5f7588e45b8a16bb2fffd252
Jared <buckey206@gmail.com>
2015-11-05 04:19:45 -0500

    added tmloader.sh

--- tmloader.sh
+++ tmloader.sh
@@ -1,9 +1,19 @@
-tmux new-window -a -n WinSplit
-tmux new-session -d -s WinSplit
-tmux selectp -t WinSplit
-tmux split-window -v
-tmux set-window-option -g window-status-current-bg blue
-tmux split-window -v
-tmux split-window -v
-tmux select-layout even-vertical
-tmux attach -t WinSplit
+tmux new-session -d -s dev
+tmux select-window -t dev:1
+tmux rename-window main
+tmux new-window -t dev:2
+tmux split-window -t dev:2 -v
+tmux split-window -t dev:2.2 -h
+tmux select-layout -t dev:2 main-horizontal
+tmux rename-window python
+tmux send-keys -t dev:2.1 "cd ~/Dropbox/python" "Enter"
+tmux send-keys -t dev:2.2 "cd ~/Dropbox/python" "Enter"
+tmux send-keys -t dev:2.3 "cd ~/Dropbox/python" "Enter"
+tmux new-window -t dev:3
+tmux select-window -t dev:3
+tmux split-window -t dev:3 -h
+tmux rename-window webdev
+tmux send-keys -t dev:3.1 "cd /var/www/" "Enter"
+tmux send-keys -t dev:3.2 "cd /var/www/" "Enter"
+tmux select-window -t dev:1
+tmux attach -t dev
